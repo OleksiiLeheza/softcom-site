fb0d792a62620a063f77bbfc58c03b317c519164
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-08-09 01:53:38 +0300

    add flex properties to active menu

--- java script/script.js
+++ java script/script.js
@@ -1,29 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.getElementById("menuButton");
-  const dropdown_menu = document.querySelector(".dropdown_menu");
-  const title = document.getElementById("title");
-
-  menuButton.addEventListener("click", () => {
-    isMenuOpened = dropdown_menu.classList.contains("active") ? true : false;
-    dropdown_menu.classList.toggle("active");
-    title.classList.toggle("menu");
-    adjustBoxPosition(isMenuOpened, calculateDropMenuMarginHeight());
-  });
+  const dropedMenu = document.querySelector(".dropdown_menu");
+  menuButton.addEventListener("mouseenter", useDropdownMenu);
+  dropedMenu.addEventListener("mouseleave", useDropdownMenu);
 });
 
+function useDropdownMenu() {
+  const dropdown_menu = document.querySelector(".dropdown_menu");
+  const title = document.getElementById("title");
+  dropdown_menu.classList.toggle("active");
+  title.classList.toggle("active");
+  isMenuOpened = dropdown_menu.classList.contains("active") ? true : false;
+  adjustBoxPosition(isMenuOpened, calculateDropMenuMarginHeight);
+}
+
 function calculateDropMenuMarginHeight() {
-  const menu = document.querySelector(".dropdown_menu");
+  var menu = document.querySelector(".dropdown_menu");
   if (menu.classList.contains("active")) {
-    return document.querySelector(".dropdown_menu").offsetHeight;
+    menu = document.querySelector(".dropdown_menu .active");
+    return menu.offsetHeight + menu.style.marginTop;
+  } else {
+    return 0;
   }
-  return 0;
 }
 
 function adjustBoxPosition(isMenuOpened, height) {
   const title = document.getElementById("title");
 
   if (isMenuOpened === true) {
-    title.style.marginTop = 20 + "px";
+    title.style.marginTop = height + "px";
   } else {
     title.style.marginTop = height + "px";
   }
