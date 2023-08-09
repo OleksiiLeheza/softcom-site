bf34c308780f6bc2242c17e4dca019faf3ed3571
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-08-09 03:41:51 +0300

    add animation when object appear

--- java script/script.js
+++ java script/script.js
@@ -5,13 +5,31 @@ document.addEventListener("DOMContentLoaded", () => {
   dropedMenu.addEventListener("mouseleave", useDropdownMenu);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.getElementById("menuButton");
+  menuButton.addEventListener("onclick", useDropdownMenu);
+});
+
 function useDropdownMenu() {
   const dropdown_menu = document.querySelector(".dropdown_menu");
   const title = document.getElementById("title");
+  const titleDescription = document.getElementById("title_description");
+  const actives = document.querySelectorAll(".active");
+
   dropdown_menu.classList.toggle("active");
   title.classList.toggle("active");
+  titleDescription.classList.toggle("active");
   isMenuOpened = dropdown_menu.classList.contains("active") ? true : false;
   adjustBoxPosition(isMenuOpened, calculateDropMenuMarginHeight);
+  if (dropdown_menu.classList.contains("active")) {
+    actives.forEach((active) => {
+      active.classList.add("fadeIn");
+    });
+  } else {
+    dropdown_menu.classList.add("fadeIn");
+    title.classList.add("moveDown");
+    titleDescription.classList.add("moveDown");
+  }
 }
 
 function calculateDropMenuMarginHeight() {
